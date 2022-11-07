b4510acd94bbd74b2dcc9e25e31ee32a64a4cee8
eswww <dd@e;sd.com>
2022-11-07 22:23:06 +0300

    add Categorys Response

---
+++ E_Shop_API/Responses/СategoryResponses/GetCategorysResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace E_Shop_API.Responses.СategoryResponses
+{
+    public class GetCategorysResponse
+    {
+        public int CategoryTotalCount { get; set; }
+        public List<GetCategorysResponseItem> Categorys { get; set; }
+    }
+}
---
+++ E_Shop_API/Responses/СategoryResponses/GetCategorysResponseItem.cs
@@ -0,0 +1,8 @@
+﻿namespace E_Shop_API.Responses.СategoryResponses
+{
+    public class GetCategorysResponseItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+    }
+}
