cfa23819ec75bbc7d0427d14e16d8b940983c4de
eswww <dd@e;sd.com>
2022-11-07 21:45:59 +0300

    add Get for return GetProductResponse

--- E_Shop_API/Controllers/ProductsController.cs
+++ E_Shop_API/Controllers/ProductsController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using E_Shop_API.Responses.ProductResponses;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace E_Shop_API.Controllers
 {
@@ -12,6 +14,40 @@ namespace E_Shop_API.Controllers
         public ProductsController(EShopDbContext context)
         {
             _context = context;
+        }
+
+        [HttpGet]
+        public async Task<GetProductResponse> Get()
+        {
+            // создание запроса и внутри него "сопоставление полей"
+            var query = _context.Products
+                .Select(x => new GetProductResponseItem()
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    ImageURL = x.ImageURL,
+                    Price = x.Price,
+                    Manufacture = new GetProductResponseItemManufacture()
+                    {
+                        Id = x.Manufacture!.Id,
+                        Name = x.Manufacture!.Name,
+                    },
+                    Category = new GetProductResponseItemCategory()
+                    {
+                        Id = x.Сategory!.Id,
+                        Name=x.Сategory!.Name,
+                    }
+                });
+
+            var productTotalCount = await query.CountAsync();
+            var products = await query.ToListAsync();
+
+            return new GetProductResponse()
+            {
+                ProductTotalCount = productTotalCount,
+                Products = products,
+            };
+
         }
     }
 }
