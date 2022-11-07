da4a2e5191d1983a453c1620d9afa9936dd91ed4
eswww <dd@e;sd.com>
2022-11-07 22:24:29 +0300

    Creating new ManufacturersController

---
+++ E_Shop_API/Controllers/ManufacturersController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Shop_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ManufacturersController : ControllerBase
+    {
+    }
+}
