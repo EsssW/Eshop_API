c1a4f512fd3b4f9a249deddce523497572c75603
eswww <dd@e;sd.com>
2022-11-07 22:23:43 +0300

    creating Manufacturers Response

---
+++ E_Shop_API/Responses/ManufactureResponses/GetManufacturersResponse.cs
@@ -0,0 +1,10 @@
+﻿using E_Shop_API.Responses.СategoryResponses;
+
+namespace E_Shop_API.Responses.ManufactureResponses
+{
+    public class GetManufacturersResponse
+    {
+        public int ManufactureTotalCount { get; set; }
+        public List<GetManufacturersResponseItem>? Manufacturers { get; set; }
+    }
+}
---
+++ E_Shop_API/Responses/ManufactureResponses/GetManufacturersResponseItem.cs
@@ -0,0 +1,8 @@
+﻿namespace E_Shop_API.Responses.ManufactureResponses
+{
+    public class GetManufacturersResponseItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+    }
+}
