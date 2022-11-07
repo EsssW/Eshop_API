1be19e360b2c2c39cab43aa683d6a105f080348e
eswww <dd@e;sd.com>
2022-11-07 21:27:49 +0300

    Creating ProductsControllers

---
+++ E_Shop_API/Controllers/ProductsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Shop_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly EShopDbContext _context;
+
+        public ProductsController(EShopDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
