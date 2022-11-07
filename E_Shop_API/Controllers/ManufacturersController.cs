09fb2195ec5a167e7311c20b70657f99752dc027
eswww <dd@e;sd.com>
2022-11-07 22:30:41 +0300

    create GET in ManufacturersController

--- E_Shop_API/Controllers/ManufacturersController.cs
+++ E_Shop_API/Controllers/ManufacturersController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using E_Shop_API.Responses.ManufactureResponses;
+using E_Shop_API.Responses.СategoryResponses;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace E_Shop_API.Controllers
 {
@@ -7,5 +10,32 @@ namespace E_Shop_API.Controllers
     [ApiController]
     public class ManufacturersController : ControllerBase
     {
+        private readonly EShopDbContext _context;
+
+        public ManufacturersController(EShopDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<GetManufacturersResponse> Get()
+        {
+            var query = _context.Manufactures
+                .Select(x => new GetManufacturersResponseItem()
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                });
+            var manufactureTotalCount = await query.CountAsync();
+            var manufacturers = await query
+                .OrderBy(x => x.Name)
+                .ToListAsync();
+
+            return new GetManufacturersResponse()
+            {
+                ManufactureTotalCount = manufactureTotalCount,
+                Manufacturers= manufacturers
+            };
+        }
     }
 }
