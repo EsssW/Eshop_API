5146894ad6685efa392fd76aa76b7d2d61684191
eswww <dd@e;sd.com>
2022-11-13 16:07:16 +0300

    create User Controller and Register post methods
    with CreatingPasswordHash and verify him

---
+++ E_Shop_API/Controllers/UserController.cs
@@ -0,0 +1,70 @@
+﻿using E_Shop_API.Models;
+using E_Shop_API.Requests.UserRequests;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Security.Claims;
+using System.Security.Cryptography;
+
+namespace E_Shop_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly EShopDbContext _context;
+
+        public UserController(EShopDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register(RegisterRequest request)
+        {
+            var isLoginUnique = _context.Users.All(x => x.Login != request.Login);
+            if (!isLoginUnique)
+                throw new Exception("User with such login already existis");
+
+            CreatePasswordHash(request.Password, out byte[] passwordHash, out byte[] passwordSalt);
+            var user = new User();
+
+            user.Login = request.Login;
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+            user.Name = request.Name;
+            user.Email = request.Email;
+            user.Phone = request.Phone;
+
+            await _context.AddAsync(user);
+            await _context.SaveChangesAsync();
+
+            return Ok(user.Id);
+        }
+
+
+        #region Creae password hash and verify passhash
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return computedHash.SequenceEqual(passwordHash);
+            }
+        }
+
+        #endregion
+    }
+}
