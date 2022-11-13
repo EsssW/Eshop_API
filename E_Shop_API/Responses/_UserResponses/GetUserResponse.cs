815e637574ec664b474fdb98fa000439e3caebcb
eswww <dd@e;sd.com>
2022-11-13 16:05:40 +0300

    Create User register Response

---
+++ E_Shop_API/Responses/_UserResponses/GetUserResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace E_Shop_API.Responses._UserResponses
+{
+    public class UserResponses
+    {
+        public string Login { get; set; } = null!;
+
+        public string Name { get; set; } = null!;
+
+        public string? Phone { get; set; }
+        public string? Email { get; set; }
+    }
+}
