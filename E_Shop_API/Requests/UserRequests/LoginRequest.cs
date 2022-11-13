160394b2010ac000a1298176beba25f59740c26e
eswww <dd@e;sd.com>
2022-11-13 16:26:29 +0300

    create user login Request

---
+++ E_Shop_API/Requests/UserRequests/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace E_Shop_API.Requests.UserRequests
+{
+    public class LoginRequest
+    {
+        public string Login { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
