1869d4346c22dba0cce73ba29038621a407b9575
eswww <dd@e;sd.com>
2022-11-13 16:06:07 +0300

    create User Register Request

---
+++ E_Shop_API/Requests/UserRequests/RegisterRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace E_Shop_API.Requests.UserRequests
+{
+    public class RegisterRequest
+    {
+        public string Login { get; set; } = default!;
+        public string Password { get; set; } = default!;
+
+        public string Name { get; set; } = null!;
+
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+    }
+}
