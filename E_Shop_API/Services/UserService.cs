f68ec6b884a2833afc0e2b7e106d5c4c7d4bc7a7
eswww <dd@e;sd.com>
2022-11-14 21:43:53 +0300

    create UserService
    add IHttpContextAccessor and

---
+++ E_Shop_API/Services/UserService.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Claims;
+
+namespace E_Shop_API.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public UserService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public string GetLogin()
+        {
+            var result = string.Empty;
+            if (_httpContextAccessor.HttpContext != null)
+            {
+                result = _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.Name);
+            }
+            return result;
+        }
+    }
+}
