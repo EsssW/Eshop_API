f3f5a9f662e1b1d4234f369682a9ad5065887e44
eswww <dd@e;sd.com>
2022-11-14 22:07:06 +0300

    create UserPutRequest

---
+++ E_Shop_API/Requests/UserRequests/UserPutRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace E_Shop_API.Requests.UserRequests
+{
+    public class UserPutRequest
+    {
+        public string? Name { get; set; }
+
+        public string? Phone { get; set; }
+
+        public string? Email { get; set; }
+    }
+}
