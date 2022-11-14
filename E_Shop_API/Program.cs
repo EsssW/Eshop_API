d6ad2e2d1fe5721d3cb193533382a41698889112
eswww <dd@e;sd.com>
2022-11-14 21:20:35 +0300

    AddAuthentication and change CORS cnt

--- E_Shop_API/Program.cs
+++ E_Shop_API/Program.cs
@@ -1,18 +1,34 @@
 using E_Shop_API;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddCors(options => options.AddPolicy(name: "EShopOrigns",
-    policy=>{
-        policy.WithOrigins("http://localhost:4200/").AllowAnyMethod().AllowAnyHeader();
-    }
-));
+//builder.Services.AddCors(options => options.AddPolicy(name: "EShopOrigns",
+//    policy=>{
+//        policy.WithOrigins("http://localhost:4200/").AllowAnyMethod().AllowAnyHeader();
+//    }
+//));
 
 var connString = builder.Configuration.GetConnectionString("E_Shop");
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8
+                .GetBytes(builder.Configuration.GetSection("AppSettings:Token").Value)),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
 
 builder.Services.AddDbContext<EShopDbContext>(opt =>
 {
@@ -31,9 +47,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+//app.UseCors("EShopOrigns");
+app.UseCors(
+  options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()
+);
 app.UseHttpsRedirection();
 
-app.UseCors("EShopOrigns");
 
 app.UseAuthorization();
 
