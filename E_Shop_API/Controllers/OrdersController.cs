679b7bf875b40aead581b8ed89171f597163defb
eswww <dd@e;sd.com>
2022-11-24 20:27:01 +0300

    creating Orders Controller

---
+++ E_Shop_API/Controllers/OrdersController.cs
@@ -0,0 +1,23 @@
+﻿using E_Shop_API.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Shop_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly EShopDbContext _context;
+        private readonly IUserService _userService;
+
+        public OrdersController(
+            EShopDbContext context,
+            IUserService userService)
+        {
+            _context = context;
+            _userService = userService;
+        }
+
+    }
+}
