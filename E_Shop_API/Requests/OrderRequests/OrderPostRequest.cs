d0d1c0a5ee6c41679650c802e61da50ea4644a96
eswww <dd@e;sd.com>
2022-11-24 20:48:22 +0300

    creatin main Order Post Request
    for using in Order Controller Put Methods

---
+++ E_Shop_API/Requests/OrderRequests/OrderPostRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace E_Shop_API.Requests.OrderRequests
+{
+    public class OrderPostRequest
+    {
+        public string Addres { get; set; } = default!;
+
+        public List<OrderPostRequestOrderItem> OrderItems { get; set; }
+    }
+}
