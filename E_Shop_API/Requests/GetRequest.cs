f4e11c9962a630c485dc5c0e914325b37a4dc642
eswww <dd@e;sd.com>
2022-11-24 19:20:36 +0300

    Creating GetRequest Class to using Requests

---
+++ E_Shop_API/Requests/GetRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace E_Shop_API.Requests
+{
+    public class GetRequest
+    {
+        public int Page { get; set; } = 1;
+        public int Limit { get; set; }
+
+        public string OrderBy { get; set; } = "Id";
+
+        public bool IsAscending { get; set; } = true;
+    }
+}
