eb19d5df8b876da77e2995bef18680e94c2cb38a
eswww <dd@e;sd.com>
2022-11-24 19:22:32 +0300

    creating GET Product Request

---
+++ E_Shop_API/Requests/ProductRequests/ProductGetRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace E_Shop_API.Requests.ProductRequests
+{
+    public class ProductGetRequest
+    {
+        public string? SearchQuery { get; set; }
+
+        public List<int>? ManufactureFilters { get; set; }
+        public List<int>? CategoryFilters { get; set; }
+        
+    }
+}
