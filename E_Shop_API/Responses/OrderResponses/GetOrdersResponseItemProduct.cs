9006cbbab9710e589707cac439433baf50f22794
eswww <dd@e;sd.com>
2022-11-24 20:32:46 +0300

    create Get Orders Response Item Product

---
+++ E_Shop_API/Responses/OrderResponses/GetOrdersResponseItemProduct.cs
@@ -0,0 +1,10 @@
+﻿namespace E_Shop_API.Responses.OrderResponses
+{
+    public class GetOrdersResponseItemProduct
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string ImageURL { get; set; } = default!;
+        public int Price { get; set; }
+    }
+}
