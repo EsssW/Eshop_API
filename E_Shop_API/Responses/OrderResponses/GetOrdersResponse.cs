f2b4ec557e2616ca8bd9cae02808ceda91f634f4
eswww <dd@e;sd.com>
2022-11-24 20:33:48 +0300

    Get Orders Response

---
+++ E_Shop_API/Responses/OrderResponses/GetOrdersResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace E_Shop_API.Responses.OrderResponses
+{
+    public class GetOrdersResponse
+    {
+        public int OrderTotalCount { get; set; }
+        public List<GetOrdersResponseItem>? Items { get; set; }
+    }
+}
