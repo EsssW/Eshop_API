61a5f67d68fcd83b0a4fe4d8e04850cb1cbe0ca1
eswww <dd@e;sd.com>
2022-11-24 20:33:25 +0300

    create Get Orders Response Item Order Item

---
+++ E_Shop_API/Responses/OrderResponses/GetOrdersResponseItemOrderItem.cs
@@ -0,0 +1,8 @@
+﻿namespace E_Shop_API.Responses.OrderResponses
+{
+    public class GetOrdersResponseItemOrderItem
+    {
+        public int Id { get; set; }
+        public GetOrdersResponseItemProduct Product { get; set; }
+    }
+}
